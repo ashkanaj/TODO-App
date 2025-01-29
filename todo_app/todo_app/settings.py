31a4eb2276764871cc92180516a1903ae3c210f1
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-01-29 10:31:19 +0330

    todo-app

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_app_db',
+        'USER': 'root',
+        'PASSWORD': '1234Abcd',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
