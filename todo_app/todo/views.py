f7751df779bfdeb8ba1ffd8326866747f26ff854
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-01-29 11:21:38 +0330

    todo-app

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all().
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     search_fields = ['title', 'description']
