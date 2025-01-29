bd1e140374d86b6ffc588908a05513576d2743f8
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-01-29 11:38:03 +0330

    todo-app

--- todo_app/todo/urls.py
+++ todo_app/todo/urls.py
@@ -1,9 +1,28 @@
-from django.db import router
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import TaskViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
-urlpatterns = [path('', include(router.urls))]
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todo API",
+        default_version='v1',
+        description="API documentation for Todo App",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="ashkanaj10000@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-2^hu9ab@*d!_-oti8ikx*dnqnd+c9krtfwjc#!mz#$@9elp2j!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -79,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todo_app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -93,8 +90,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -114,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -125,7 +119,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
